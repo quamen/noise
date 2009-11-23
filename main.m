b64752ef217540b8bc0acb086f5b31b5b2b933f4
Gareth Townsend <gareth.townsend@me.com>
2009-11-24 09:42:52 +1100

    MacRuby Project.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  noise
+//
+//  Created by Gareth Townsend on 24/11/09.
+//  Copyright Clear Interactive 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
